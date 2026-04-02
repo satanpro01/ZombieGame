5b1e32b3ae6cffcb8a3e485fa4b7cc2c487bba3c
satanpro01 <satanpro01@gmail.com>
2026-04-02 12:36:19 +0900

    add yetanothernet, change to client replication

--- Packages/matter.lua
+++ Packages/matter.lua
@@ -1,1 +1,4 @@
-return require(script.Parent._Index["matter-ecs_matter@0.8.5"]["matter"])
+local REQUIRED_MODULE = require(script.Parent._Index["matter-ecs_matter@0.8.5"]["matter"])
+export type World = REQUIRED_MODULE.World 
+export type Loop = REQUIRED_MODULE.Loop 
+return REQUIRED_MODULE
